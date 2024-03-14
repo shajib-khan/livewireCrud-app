de48b9d59e7be0e8455a50ec316eecfdde4d4391
shajib-khan <shajibkhan946@gmail.com>
2024-03-14 12:58:57 +0600

    database table migrated

--- database/migrations/2024_03_14_065345_create_comments_table.php
+++ database/migrations/2024_03_14_065345_create_comments_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('comments', function (Blueprint $table) {
             $table->id();
+            $table->integer('post_id');
+            $table->integer('message');
             $table->timestamps();
         });
     }
